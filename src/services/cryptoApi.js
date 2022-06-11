81eeab63ca7a3d9e8fc297fe109c583d5e6f8d1e
abdulsagheer <abdulsagheeras29@gmail.com>
2022-06-11 10:44:30 +0530

    added crypto components

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,28 +1,41 @@
-const axios = require("axios");
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  url: "https://coinranking1.p.rapidapi.com/coins",
-  params: {
-    referenceCurrencyUuid: "yhjMzLPhuIDl",
-    timePeriod: "24h",
-    "tiers[0]": "1",
-    orderBy: "marketCap",
-    orderDirection: "desc",
-    limit: "50",
-    offset: "0",
-  },
-  headers: {
-    "X-RapidAPI-Key": "78c5a77459msh4f149f5ef9f90e4p12113fjsnaa290edcffcc",
-    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-  },
+// Note: Change v1 to v2 on rapid api
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
